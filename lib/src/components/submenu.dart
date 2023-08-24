076c57c557b8ae0deccbf514a50c797002dbe2fb
Thiago Carvalho <thiagocarvalho.dev@gmail.com>
2023-08-24 09:12:59 -0300

    feat(submenu)
    implement the new component `ArDriveSubmenu`.

---
+++ lib/src/components/submenu.dart
@@ -0,0 +1,147 @@
+import 'package:ardrive_ui/ardrive_ui.dart';
+import 'package:flutter/material.dart';
+
+class ArDriveSubmenuItem {
+  final Widget widget;
+  final List<ArDriveSubmenuItem>? children;
+  final Function? onClick;
+  final MenuController menuController = MenuController();
+
+  ArDriveSubmenuItem({required this.widget, this.children, this.onClick});
+}
+
+class ArDriveSubmenu extends StatefulWidget {
+  const ArDriveSubmenu({
+    super.key,
+    required this.child,
+    required this.menuChildren,
+    this.alignmentOffset = Offset.zero,
+  });
+
+  final Widget child;
+  final List<ArDriveSubmenuItem> menuChildren;
+  final Offset alignmentOffset;
+
+  @override
+  State<ArDriveSubmenu> createState() => _ArDriveSubmenuState();
+}
+
+class _ArDriveSubmenuState extends State<ArDriveSubmenu> {
+  final topMenuController = MenuController();
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ArDriveMenuWidget(
+      menuController: topMenuController,
+      menuChildren: _buildMenu(widget.menuChildren),
+      alignmentOffset: widget.alignmentOffset,
+      child: widget.child,
+    );
+  }
+
+  List<Widget> _buildMenu(List<ArDriveSubmenuItem> menuChildren) {
+    List<Widget> children = [];
+
+    for (final element in menuChildren) {
+      if (element.children == null) {
+        children.add(ArDriveMenuWidget(
+          onClick: () {
+            element.onClick?.call();
+            topMenuController.close();
+          },
+          parentMenuController: topMenuController,
+          menuController: element.menuController,
+          menuChildren: const [],
+          child: element.widget,
+        ));
+        continue;
+      }
+      children.add(_buildMenuItem(element, element.menuController));
+    }
+    return children;
+  }
+
+  Widget _buildMenuItem(
+      ArDriveSubmenuItem item, MenuController menuController) {
+    return ArDriveMenuWidget(
+      onClick: () {
+        item.onClick?.call();
+        menuController.close();
+      },
+      parentMenuController: menuController,
+      menuController: item.menuController,
+      menuChildren: item.children!.map((e) {
+        if (e.children == null) {
+          return ArDriveMenuWidget(
+            onClick: () {
+              e.onClick?.call();
+              topMenuController.close();
+            },
+            parentMenuController: item.menuController,
+            menuController: e.menuController,
+            menuChildren: const [],
+            child: e.widget,
+          );
+        }
+        return _buildMenuItem(e, e.menuController);
+      }).toList(),
+      child: ArDriveClickArea(child: item.widget),
+    );
+  }
+}
+
+class ArDriveMenuWidget extends StatefulWidget {
+  const ArDriveMenuWidget({
+    super.key,
+    required this.child,
+    required this.menuChildren,
+    this.parentMenuController,
+    required this.menuController,
+    this.onClick,
+    this.alignmentOffset = Offset.zero,
+  });
+
+  final Widget child;
+  final List<Widget> menuChildren;
+  final MenuController? parentMenuController;
+  final MenuController menuController;
+  final Function? onClick;
+  final Offset alignmentOffset;
+
+  @override
+  State<ArDriveMenuWidget> createState() => _ArDriveMenuWidgetState();
+}
+
+class _ArDriveMenuWidgetState extends State<ArDriveMenuWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return MenuAnchor(
+      alignmentOffset: widget.alignmentOffset,
+      controller: widget.menuController,
+      menuChildren: widget.menuChildren,
+      child: GestureDetector(
+        onTap: () {
+          widget.onClick?.call();
+          if (widget.menuChildren.isEmpty &&
+              widget.parentMenuController != null) {
+            widget.parentMenuController!.close();
+            return;
+          }
+
+          if (widget.menuController.isOpen) {
+            widget.menuController.close();
+            return;
+          }
+
+          widget.menuController.open();
+        },
+        child: ArDriveClickArea(child: widget.child),
+      ),
+    );
+  }
+}
